aea0b5e480b84548581c3e75fde22b22e66cd155
Jeremy Bidet <jeremy.bidet@gmail.com>
2017-03-06 20:20:05 +0100

    add base project : makefile and empty main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+/* 
+ * File:   main.cpp
+ * Author: jerem
+ *
+ * Created on 6 mars 2017, 20:06
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
     return 0;
 }
+
