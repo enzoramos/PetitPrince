ea0f6346299e8b2c8392562d6c5d2dae1ca7526d
jeremy <jeremy.bidet@gmail.com>
2017-03-10 11:53:25 +0100

    update sources location add project pdf states

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -35,9 +35,9 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/Serveur.o \
-	${OBJECTDIR}/ServiceDrawSK.o \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/src/Serveur.o \
+	${OBJECTDIR}/src/ServiceDrawSK.o
 
 
 # C Compiler Flags
@@ -64,20 +64,20 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/petitprince: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/petitprince ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/Serveur.o: Serveur.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Serveur.o Serveur.cpp
-
-${OBJECTDIR}/ServiceDrawSK.o: ServiceDrawSK.cc 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ServiceDrawSK.o ServiceDrawSK.cc
-
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/src/Serveur.o: src/Serveur.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/Serveur.o src/Serveur.cpp
+
+${OBJECTDIR}/src/ServiceDrawSK.o: src/ServiceDrawSK.cc 
+	${MKDIR} -p ${OBJECTDIR}/src
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/ServiceDrawSK.o src/ServiceDrawSK.cc
 
 # Subprojects
 .build-subprojects:
